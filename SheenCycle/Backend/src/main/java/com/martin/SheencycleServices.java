bbec9a6fe934c1c389302fb75c7a776ad1d509c0
MARTIN <muniumartinihugo@gmail.com>
2021-01-22 13:05:22 +0300

    Updates

---
+++ SheenCycle/Backend/src/main/java/com/martin/SheencycleServices.java
@@ -0,0 +1,12 @@
+package com.martin;
+
+import io.vertx.core.AbstractVerticle;
+
+public class SheencycleServices extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
