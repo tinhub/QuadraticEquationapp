1b48187ca3eb28de5671a377f3a4e2f9a22eb37b
MARTIN <muniumartinihugo@gmail.com>
2021-01-28 12:46:06 +0300

    vertex plugin

---
+++ Hamscuisine/backend/src/main/java/com/martin/hamscuisineservice.java
@@ -0,0 +1,12 @@
+package com.martin;
+
+import io.vertx.core.AbstractVerticle;
+
+public class hamscuisineservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
